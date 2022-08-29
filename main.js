0b20f02120a3ae8d80d98257c0d8227eab70bf49
jeson_lam <jeson_lam@163.com>
2022-08-29 10:02:36 +0800

    相关配置

--- main.js
+++ main.js
@@ -14,11 +14,39 @@ app.$mount()
 // #endif
 
 // #ifdef VUE3
-import { createSSRApp } from 'vue'
+import {
+    createSSRApp
+} from 'vue'
 export function createApp() {
-  const app = createSSRApp(App)
-  return {
-    app
-  }
+    const app = createSSRApp(App)
+    return {
+        app
+    }
 }
 // #endif
+
+// Vue 2 转 Vue 3, 在 main.js 中写入以下代码即可
+function isPromise(obj) {
+    return (
+        !!obj &&
+        (typeof obj === "object" || typeof obj === "function") &&
+        typeof obj.then === "function"
+    );
+}
+
+uni.addInterceptor({
+    returnValue(res) {
+        if (!isPromise(res)) {
+            return res;
+        }
+        return new Promise((resolve, reject) => {
+            res.then((res) => {
+                if (res[0]) {
+                    reject(res[0]);
+                } else {
+                    resolve(res[1]);
+                }
+            });
+        });
+    },
+});
